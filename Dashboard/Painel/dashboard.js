0ee50a13616bfd3af16e7cdba73c420f9d9a9f23
Willian Quirino <willianqf1996@gmail.com>
2023-04-30 23:00:05 -0300

    Act Branch

---
+++ Dashboard/Painel/dashboard.js
@@ -0,0 +1,26 @@
+let lista = document.querySelectorAll('.navigation li')
+// HOVER ///
+let toggle = document.querySelector('.toggle')
+let navigation = document.querySelector('.navigation')
+let main = document.querySelector('.main')
+
+toggle.onclick = function()
+{
+    navigation.classList.toggle('active')
+    main.classList.toggle('active')
+}
+
+
+function AtivarLink()
+{
+    lista.forEach((item) =>
+    item.classList.remove('hovered'))
+    this.classList.add('hovered')
+}
+
+function test()
+{
+    console.log('clicou')
+}
+
+lista.forEach((item) => item.addEventListener('mouseover', AtivarLink))
